15cb2c0033e9cbb530293057c3ffaa8b910693fd
Nicholas <nciechanowski@staff.ventraip.com>
2020-10-11 22:28:52 +1100

    /PHP/8 kyu/Even or Odd

---
+++ PHP/8 kyu/Even or Odd/test.php
@@ -0,0 +1,8 @@
+class EvenOrOddTest extends TestCase {
+  public function testExamples() {
+    $this->assertEquals("Even", even_or_odd(2));
+    $this->assertEquals("Even", even_or_odd(0));
+    $this->assertEquals("Odd", even_or_odd(7));
+    $this->assertEquals("Odd", even_or_odd(1));
+  }
+}
