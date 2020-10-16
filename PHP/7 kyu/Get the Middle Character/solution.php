d1065c8e795a97fb5e94f7388e2e7f91b836badd
Nicholas Ciechanowski <nciechanowski@staff.ventraip.com>
2020-10-16 11:17:22 +1100

    Fixed /PHP/7 kyu/Get the Middle Character

--- PHP/7 kyu/Get the Middle Character/solution.php
+++ PHP/7 kyu/Get the Middle Character/solution.php
@@ -3,7 +3,7 @@ function getMiddle($text) {
   $length = strlen($text);
   
   if ($length % 2 == 0) {
-      return substr($text, $length / 3, 2);
+      return substr($text, $length / 2.5, 2);
   } 
   
   if ($length % 2 != 0) {
