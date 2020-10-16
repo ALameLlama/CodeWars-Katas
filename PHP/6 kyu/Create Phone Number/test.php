fd3006551d51bad6ff9a15fa4c2bdad47a95e578
Nicholas Ciechanowski <nciechanowski@staff.ventraip.com>
2020-10-16 12:11:10 +1100

    /PHP/6 kyu/Create Phone Number

---
+++ PHP/6 kyu/Create Phone Number/test.php
@@ -0,0 +1,6 @@
+class MyTestCases extends TestCase {
+  public function testBasicTests() {
+    $this->assertEquals('(123) 456-7890', createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+    $this->assertEquals('(111) 111-1111', createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]));
+  }
+}
