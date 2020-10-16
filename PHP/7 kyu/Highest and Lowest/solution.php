34500779faaff17106ef62e31aa09de8269772c6
Nicholas Ciechanowski <nciechanowski@staff.ventraip.com>
2020-10-16 12:15:21 +1100

    /PHP/7 kyu/Highest and Lowest

---
+++ PHP/7 kyu/Highest and Lowest/solution.php
@@ -0,0 +1,6 @@
+function highAndLow($numbers)
+{
+  $numbers = explode(' ', $numbers);
+
+  return max($numbers) . ' ' . min($numbers);
+}
---
+++ PHP/7 kyu/Highest and Lowest/test.php
@@ -0,0 +1,6 @@
+class MyTestCases extends TestCase
+{
+    public function testSampleTests() {
+      $this->assertEquals("42 -9", highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+    }
+}
